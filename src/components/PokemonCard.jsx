67d38f753f19dcba7f8a90968b1160577cfb90f9
Jerome Desouches <jerome.desouches@gmail.com>
2023-05-30 22:14:24 +0200

    react Basic 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,27 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
+
 const PokemonCard = () => {
-    return(
+
+    
+    const pokemon = pokemonList[0].name;
+    const pokemonImage =  pokemonList.find(element => element.name === pokemon).imgSrc;
+    
+
+    return (
     <div>
-        <figure figcaption="Bulbasaur">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
+        <figure figcaption={pokemon} >
+            <img src= {pokemonImage}></img>
         </figure>
     </div>
     );
