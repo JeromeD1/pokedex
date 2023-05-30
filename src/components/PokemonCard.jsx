9626b4bd78c90258f4417cfc314a099e774bbc08
Jerome Desouches <jerome.desouches@gmail.com>
2023-05-30 17:11:12 +0200

    Ajout PokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,11 @@
+const PokemonCard = () => {
+    return(
+    <div>
+        <figure figcaption="Bulbasaur">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
+        </figure>
+    </div>
+    );
+}
+
+export default PokemonCard;
